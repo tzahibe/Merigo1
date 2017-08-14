53e4afc3efb5b43fa810825d051295d20f94dc32
צחי בן חמו <tzachi222@Gmail.com>
2017-08-14 14:57:40 +0300

    Add project files.

---
+++ Merigo/Scripts/myApp/myApp.js
@@ -0,0 +1,24 @@
+﻿var myApp = angular.module('myApp', ['angular-carousel']);
+
+myApp.controller('FormCtrl', ['$scope',
+    function ($scope) {
+        var self = this;
+        $scope.user = {};
+        $scope.slides3 = [
+            { id: 1, img: '/Content/images/leket2_61161_0.png', text: 'אתר נחיתה למד"א'},
+            { id: 2, img: "/Content/images/mda2_61167_0.png", text: "אתר מדרשת"},
+            { id: 3, img: "/Content/images/midreshet2_61165_0.png", text: "אתר נטבוק" },
+            { id: 4, img: "/Content/images/netbook2_61166_0.png", text: "אתר סטודנטים לאונ' הפתוחה"},
+        ];
+   
+    console.log($scope.slides3);
+    self.init = function () {
+       
+    }
+
+    $scope.submit = function () {
+        $scope.formSubmitted = true;
+    }
+
+    self.init();
+}]);
