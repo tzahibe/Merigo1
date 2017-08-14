d3a01b0021f8b7f4a2e50a5569debfbbdf267cf3
צחי בן חמו <tzachi222@Gmail.com>
2017-08-14 15:02:53 +0300

    שדג שד

--- Merigo/Scripts/myApp/myApp.js
+++ Merigo/Scripts/myApp/myApp.js
@@ -9,6 +9,7 @@ myApp.controller('FormCtrl', ['$scope',
             { id: 2, img: "/Content/images/mda2_61167_0.png", text: "אתר מדרשת"},
             { id: 3, img: "/Content/images/midreshet2_61165_0.png", text: "אתר נטבוק" },
             { id: 4, img: "/Content/images/netbook2_61166_0.png", text: "אתר סטודנטים לאונ' הפתוחה"},
+            { id: 5, img: "/Content/images/Screenshot_1.png", text: "כולל חזון עובדיה" },
         ];
    
     console.log($scope.slides3);
